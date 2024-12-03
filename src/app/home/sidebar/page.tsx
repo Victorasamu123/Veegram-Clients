086de27714a49ec40cd26b8a6ac7bbdbfc483528
Victorasamu123 <asamuvictor03@gmail.com>
2024-12-03 15:29:33 +0100

    updated addtocart controller

--- src/app/home/sidebar/page.tsx
+++ src/app/home/sidebar/page.tsx
@@ -11,10 +11,10 @@ import { IoCreateOutline } from "react-icons/io5";
 import { IoSettingsOutline } from "react-icons/io5";
 import { MdLogout } from "react-icons/md";
 import profile from "../../../../public/test p.jpg"
-import { Open_Sans } from 'next/font/google'
+import { Roboto } from 'next/font/google'
 
 
-const notoSans = Open_Sans({
+const notoSans = Roboto({
   subsets:['latin'],
 })
 export default function SlideBarOne() {
@@ -24,62 +24,62 @@ export default function SlideBarOne() {
        <div className="w-full pt-10 pl-8">
         <Link href='' className="flex">
         <GoHome className="text-[29px]"/>
-        <p className={`${notoSans.className} text-[18px] font-apple font-semibold mt-[2px] ml-2`}>Home</p>
+        <p className={`${notoSans.className} text-[18px] font-semibold mt-[2px] ml-2`}>Home</p>
         </Link>
        </div>
        <div className="w-full pt-6 pl-8">
          <Link href='' className="flex">
          <IoPeopleOutline className="text-[29px]"/>
-          <p className={`${notoSans.className} text-[18px] font-apple font-semibold mt-[2px] ml-2`}>Friends</p>
+          <p className={`${notoSans.className} text-[18px] font-semibold mt-[2px] ml-2`}>Friends</p>
          </Link>
        </div>
        <div className="w-full pt-6 pl-8">
         <Link href="" className="flex">
           <IoSearch className="text-[29px]"/>
-          <p className={`${notoSans.className} text-[18px] font-apple font-semibold mt-[2px] ml-2`}>Search</p>
+          <p className={`${notoSans.className} text-[18px] font-semibold mt-[2px] ml-2`}>Search</p>
         </Link>
        </div>
        <div className="w-full pt-6 pl-8">
         <Link href='' className="flex">
           <MdOutlineVideoLibrary className="text-[29px]"/>
-          <p className={`${notoSans.className} text-[18px] font-apple font-semibold mt-[2px] ml-2`}>Clips</p>
+          <p className={`${notoSans.className} text-[18px] font-semibold mt-[2px] ml-2`}>Clips</p>
         </Link>
        </div>
        <div className="w-full pt-6 pl-8">
         <Link href='' className="flex">
         <RiMessage3Line className="text-[29px]"/>
-        <p className={`${notoSans.className} text-[18px] font-apple font-semibold mt-[2px] ml-2`}>Messages</p>
+        <p className={`${notoSans.className} text-[18px] font-semibold mt-[2px] ml-2`}>Messages</p>
         </Link>
        </div>
        <div className="w-full pt-6 pl-8">
         <Link href='' className="flex">
         <IoIosNotificationsOutline className="text-[29px]"/>
-        <p className={`${notoSans.className} text-[18px] font-apple font-semibold mt-[2px] ml-2`}>Notifications</p>
+        <p className={`${notoSans.className} text-[18px] font-semibold mt-[2px] ml-2`}>Notifications</p>
         </Link>
        </div>
        <div className="w-full pt-6 pl-8">
         <Link href='' className="flex">
         <IoCreateOutline className="text-[29px]"/>
-        <p className={`${notoSans.className} text-[18px] font-apple font-semibold mt-[2px] ml-2`}>Create</p>
+        <p className={`${notoSans.className} text-[18px] font-semibold mt-[2px] ml-2`}>Create</p>
         </Link>
        </div>
        <div className="w-full pt-6 pl-8">
         <Link href='' className="flex">
         <Image src={profile} alt="" width={35} className="rounded-[50%]"/>
-        <p className={`${notoSans.className} text-[18px] font-apple font-semibold mt-[2px] ml-2`}>Profile</p>
+        <p className={`${notoSans.className} text-[18px] font-semibold mt-[2px] ml-2`}>Profile</p>
         </Link>
        </div>
        <div className="mt-24">
          <div className="w-full pt-6 pl-8">
           <Link href='' className="flex">
           <IoSettingsOutline className="text-[29px]"/>
-          <p className={`${notoSans.className} text-[18px] font-apple font-semibold mt-[2px] ml-2`}>Settings</p>
+          <p className={`${notoSans.className} text-[18px] font-semibold mt-[2px] ml-2`}>Settings</p>
           </Link>
          </div>
          <div className="w-full pt-6 pl-8">
           <Link href='' className="flex">
           <MdLogout className="text-[29px]"/>
-          <p className="text-[18px] font-apple font-semibold mt-[2px] ml-2">Logout</p>
+          <p className="text-[18px] font-semibold mt-[2px] ml-2">Logout</p>
           </Link>
          </div>
        </div>
